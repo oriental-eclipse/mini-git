9375622ee4a56eb486d0c305353866f927f1afdc
Krishna Bhosle <kvb756613@gmail.com>
2026-03-22 23:33:29 +0530

    Added readIndex Function

--- include/index.hpp
+++ include/index.hpp
@@ -1,6 +1,13 @@
 #ifndef INDEX_HPP
 #define INDEX_HPP
 
-#include "../include/index.hpp"
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <unordered_map>
+
+std::unordered_map<std::string, std::string> readIndex();
+void writeIndex(const std::unordered_map<std::string, std::string> &index);
+void stageFile(const std::string &fileName, const std::string &hash);
 
 #endif 
--- src/index.cpp
+++ src/index.cpp
@@ -1,1 +1,27 @@
- //:P
+#include "../include/index.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <unordered_map>
+
+static std::string INDEX_PATH = "../index/stage.txt";
+
+std::unordered_map<std::string, std::string> readIndex(){
+    std::unordered_map<std::string, std::string> index;
+
+    std::ifstream fin(INDEX_PATH);
+
+    if(!fin){
+        return index;
+    }
+
+    std::string file, hash;
+
+    if(fin >> file >> hash){
+        index[file] = hash;
+    }
+
+    return index;
+}
+
