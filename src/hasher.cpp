a6d350807200fb929c23e09909e42925bd0f6329
Krishna <kvb756613@gmail.com>
2026-03-30 11:42:47 -0600

    Removed commented deprecated function (fileHAsher)

--- src/hasher.cpp
+++ src/hasher.cpp
@@ -10,36 +10,6 @@ uint64_t hashingLogic(const std::string &originalString){
     return hash;
 }
 
-/*
-Blob fileHasher(const std::string &fileName){
-    std::ifstream fin;
-    std::string fileContent;
-    Blob blob;
-
-    fin.open("../data/" + fileName, std::ios::binary);
-
-    if(!fin.is_open()){
-        std::cerr << "Fatal Error : File doesn't exist / Could not be opened!\n";
-        exit(1);
-    }
-
-    std::ostringstream ss;
-    ss << fin.rdbuf();
-    fileContent = ss.str();
-
-    std::string blobHeader = "blob " + std::to_string(fileContent.size());
-    blobHeader.push_back('\0');
-    blob.content = blobHeader + fileContent;
-    std::stringstream hexer;
-    hexer << std::hex << std::setw(16) << std::setfill('0') << hashingLogic(blob.content);;
-    blob.hash = hexer.str();
-
-    fin.close();
-
-    return blob;
-}
-    */
-
 Blob fileHasher(const std::string &fileName){
     Blob blob;
 
