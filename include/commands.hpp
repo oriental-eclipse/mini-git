67458e73f9fde3b4a31876de75dd84202855213d
oriental-eclipse <orientalyt@gmail.com>
2026-07-14 01:18:16 +0530

    Added commands.hpp

---
+++ include/commands.hpp
@@ -0,0 +1,12 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <iostream>
+#include "../include/hasher.hpp"
+#include "../include/object.hpp"
+#include "../include/index.hpp"
+
+int addCommand(const int argc, const char* argv[]);
+int initCommand(const int argc, const char* argv[]);
+
+#endif
